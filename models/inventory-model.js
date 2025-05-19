8d1b803bc1daf2c0c8c848f732775eda057b7747
Vera Antamanova <vantamanova@yandex.ru>
2025-05-19 12:11:30 +0400

    W03: Inventory Delivery By Classification

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -7,4 +7,24 @@ async function getClassifications(){
   return await pool.query("SELECT * FROM public.classification ORDER BY classification_name")
 }
 
-module.exports = {getClassifications} //exports the function for use elsewhere.
+
+
+/* ***************************
+ *  Get all inventory items and classification_name by classification_id
+ * ************************** */
+async function getInventoryByClassificationId(classification_id) {
+  try {
+    const data = await pool.query(
+      `SELECT * FROM public.inventory AS i 
+      JOIN public.classification AS c 
+      ON i.classification_id = c.classification_id 
+      WHERE i.classification_id = $1`, // $1 is a placeholder
+      [classification_id]
+    )
+    return data.rows // sends the data, as an array of all the rows, back to where the function was called (in the controller)
+  } catch (error) {
+    console.error("getclassificationsbyid error " + error)
+  }
+}
+
+module.exports = {getClassifications, getInventoryByClassificationId}; //exports functions for use elsewhere.
--- utilities/index.js
+++ utilities/index.js
@@ -24,4 +24,37 @@ Util.getNav = async function (req, res, next) {
   return list
 }
 
+/* **************************************
+* Build the classification view HTML
+* ************************************ */
+Util.buildClassificationGrid = async function(data){
+  let grid // declares a variable to hold a string.
+  if(data.length > 0){ // an "if" to see if the array is not empty.
+    grid = '<ul id="inv-display">' // creates an unordered list element and adds it to the grid variable.
+    data.forEach(vehicle => { // break each element of the data array into a vehicle object
+      grid += '<li>'
+      grid +=  '<a href="../../inv/detail/'+ vehicle.inv_id 
+      + '" title="View ' + vehicle.inv_make + ' '+ vehicle.inv_model 
+      + 'details"><img src="' + vehicle.inv_thumbnail 
+      +'" alt="Image of '+ vehicle.inv_make + ' ' + vehicle.inv_model 
+      +' on CSE Motors" /></a>'
+      grid += '<div class="namePrice">'
+      grid += '<hr />'
+      grid += '<h2>'
+      grid += '<a href="../../inv/detail/' + vehicle.inv_id +'" title="View ' 
+      + vehicle.inv_make + ' ' + vehicle.inv_model + ' details">' 
+      + vehicle.inv_make + ' ' + vehicle.inv_model + '</a>'
+      grid += '</h2>'
+      grid += '<span>$' 
+      + new Intl.NumberFormat('en-US').format(vehicle.inv_price) + '</span>'
+      grid += '</div>'
+      grid += '</li>'
+    })
+    grid += '</ul>'
+  } else { 
+    grid += '<p class="notice">Sorry, no matching vehicles could be found.</p>'
+  }
+  return grid // returns the variable to the calling location.
+}
+
 module.exports = Util
