95eb010e4570623d8e24f8f186a31871fde747e1
Vera Antamanova <vantamanova@yandex.ru>
2025-05-28 11:42:21 +0400

    W04: Server-side Data Validation

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,7 +3,7 @@ const express = require("express")
 const router = new express.Router() 
 const utilities = require("../utilities/")
 const accountController = require("../controllers/accountController") // Need to build
-
+const regValidate = require('../utilities/account-validation')
 
 // Route to display the login view
 router.get("/login", utilities.handleErrors(accountController.buildLogin)); // Attention to the pass
@@ -12,6 +12,9 @@ router.get("/login", utilities.handleErrors(accountController.buildLogin)); // A
 router.get("/register", utilities.handleErrors(accountController.buildRegisteration));
 
 // Registration Route
-router.post('/register', utilities.handleErrors(accountController.registerAccount)) // need to build
+router.post('/register',
+    regValidate.registationRules(),
+    regValidate.checkRegData,
+    utilities.handleErrors(accountController.registerAccount)) // need to build
 
 module.exports = router; // exports the router object for use elsewhere
